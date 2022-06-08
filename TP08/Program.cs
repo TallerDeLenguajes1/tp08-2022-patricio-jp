05d80a2bced65f64a820d2c374a28fffcddeb636
Patricio Agustín Jimenez Paz <patojimenezpaz@gmail.com>
2022-06-08 12:28:21 -0300

    Ejercicio1 completado

---
+++ TP08/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Ingrese la ruta en donde desea trabajar: ");
+string entrada = Console.ReadLine();
+Console.Clear();
+
+while (!Directory.Exists(entrada)) {
+    Console.WriteLine("Directorio inválido. Ingrese uno válido");
+    entrada = Console.ReadLine();
+    Console.Clear();
+}
+
+List<string> ListadoArchivos = Directory.GetFiles(entrada, "*", SearchOption.AllDirectories).ToList();
+
+StreamWriter sw = new StreamWriter("index.csv");
+sw.WriteLine("Registro;Nombre de fichero;Extensión");
+
+Console.WriteLine($"Archivos dentro de \"{entrada}\":");
+for (int i = 1; i < ListadoArchivos.Count; i++) {
+    Console.WriteLine(ListadoArchivos[i]);
+    string[] archivoInfoAbsoluta = ListadoArchivos[i].Split('\\');
+    string nombreArchivo = archivoInfoAbsoluta.Last().Split('.').First();
+    string extensionArchivo = archivoInfoAbsoluta.Last().Split('.').Last();
+    sw.WriteLine(i + ";" + nombreArchivo + ";" + extensionArchivo);
+}
+
+sw.Close();
